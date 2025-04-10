4cb92c980ef4747f5fdd176500a1019e264d8f50
Felipe Carvalho <felipe.silva@assesi.com>
2025-04-10 00:12:54 -0300

    feat: mongo db connection with mongoose

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
+
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().uri().required(),
+        MONGO_AUTH_SOURCE: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
