d4c1cf3d96cf2be736128a26ec5b9bc14aae86ea
Felipe Carvalho <felipe.silva@assesi.com>
2025-04-14 01:04:42 -0300

    refactor(mobile): header actions

--- mobile/app/components/header.tsx
+++ mobile/app/components/header.tsx
@@ -3,20 +3,20 @@ import colors from 'tailwindcss/colors';
 
 import Icon from './icon';
 
+import { useAuth } from '~/hooks/use-auth';
+
 const Header = () => {
+  const { signOut } = useAuth();
+
   return (
     <View className="bg-primary h-1/6 w-full flex-row items-end justify-between px-3 pb-5">
       <View>
         <Text className="font-poppins-bold mb-1 text-lg">Hoagie Hub</Text>
       </View>
 
-      <View className="flex-row items-center gap-3">
-        <TouchableWithoutFeedback>
+      <View className="flex-row items-center gap-5">
+        <TouchableWithoutFeedback onPress={signOut}>
           <Icon name="LogOut" size={20} color={colors.zinc['900']} />
-        </TouchableWithoutFeedback>
-
-        <TouchableWithoutFeedback>
-          <Icon name="User" size={20} color={colors.zinc['900']} />
         </TouchableWithoutFeedback>
       </View>
     </View>
